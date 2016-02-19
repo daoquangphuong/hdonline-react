bebaa1a0e71fbd46a4019a6b1ddd80c3e6c1cea0
Dao Quang Phuong <phuongdq@v3s.com.vn>
2016-02-19 17:35:39 +0700

    fix menu

--- flux/js/module/app/com/app.js
+++ flux/js/module/app/com/app.js
@@ -43,13 +43,33 @@ var App = React.createClass({
             <div className="wrapper">
                 <Menu/>
                 <Menu.Fake/>
-                <div id="container-fluid">
-                    <button type="button" className="btn btn-default" >default</button>
-                    <button type="button" className="btn btn-primary" >primary</button>
-                    <button type="button" className="btn btn-success" >success</button>
-                    <button type="button" className="btn btn-info" >info</button>
-                    <button type="button" className="btn btn-warning" >warning</button>
-                    <button type="button" className="btn btn-danger" >danger</button>
+                <div className="container">
+                    <form>
+                        <button type="button" className="btn btn-default">default</button>
+                        <button type="button" className="btn btn-primary">primary</button>
+                        <button type="button" className="btn btn-success">success</button>
+                        <button type="button" className="btn btn-info">info</button>
+                        <button type="button" className="btn btn-warning">warning</button>
+                        <button type="button" className="btn btn-danger">danger</button>
+                    </form>
+                    <br/>
+                    <form>
+                        <button type="button" className="btn btn-default-outline">default</button>
+                        <button type="button" className="btn btn-primary-outline">primary</button>
+                        <button type="button" className="btn btn-success-outline">success</button>
+                        <button type="button" className="btn btn-info-outline">info</button>
+                        <button type="button" className="btn btn-warning-outline">warning</button>
+                        <button type="button" className="btn btn-danger-outline">danger</button>
+                    </form>
+                    <br/>
+                    <form>
+                        <button type="button" className="btn btn-pill btn-default-outline">default</button>
+                        <button type="button" className="btn btn-pill btn-primary-outline">primary</button>
+                        <button type="button" className="btn btn-pill btn-success-outline">success</button>
+                        <button type="button" className="btn btn-pill btn-info-outline">info</button>
+                        <button type="button" className="btn btn-pill btn-warning-outline">warning</button>
+                        <button type="button" className="btn btn-pill btn-danger-outline">danger</button>
+                    </form>
                 </div>
                 <div id="container" className="container-fluid">
                     {this.props.children}
